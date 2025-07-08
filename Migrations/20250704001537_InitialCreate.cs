a15165755bc36f91e2083336bd326bd2bafce3e8
studentavd <studentavd@ML-RefVm-842662>
2025-07-07 18:07:25 -0600

    Add project files.

---
+++ Migrations/20250704001537_InitialCreate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPostApplication.Migrations
+{
+    internal class _20250704001537_InitialCreate
+    {
+    }
+}
---
+++ Models/BlogType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlogPostApplication.Models
+{
+    public class BlogType
+    {
+        public int BlogTypeId { get; set; }
+        public int status { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
